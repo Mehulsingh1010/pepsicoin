55d55e150c676fa7e6fbd00749a6f1d4240895c5
mehulsingh1010 <mehulsingh076@gmail.com>
2025-12-24 16:28:51 +0530

    updated

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import PepsiCoinApp from '@/components/Dash'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <PepsiCoinApp/>
+    </div>
+  )
+}
+
+export default page
